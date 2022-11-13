e635e7aa3d1bbd16c5a7172d75324b4595893e61
Yuri <isyuricunha@duck.com>
2022-11-13 07:30:12 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,7 +47,7 @@ export default function MyApp({
           site_name: `From my Desk`,
           images: [
             {
-              url: `https://www.yuricunha.xyz/static/images/profile.jpeg`,
+              url: `http://localhost:3000/static/images/profile2.png`,
               alt: `Profile Picture`,
             },
           ],
