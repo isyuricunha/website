f3b2eed7c64e60138fb3879b88e0b7cf714db2f9
Yuri Cunha <isyuricunha@duck.com>
2023-08-23 19:08:54 -0300

    remove discord

--- data/aboutMe.ts
+++ data/aboutMe.ts
@@ -38,8 +38,6 @@ const info = (props: any): { input: string; return: string }[] => [
       props
     )}" rel="noopener" href="https://www.instagram.com/isyuricunha/">Instagram</a>", "<a rel="noopener" style="${style(
       props
-    )}" rel="noopener" href="https://discordapp.com/users/1018988240151253002">Discord</a>", "<a rel="noopener" style="${style(
-      props
     )}" rel="noopener" href="https://t.me/isyuricunha">Telegram</a>", "<a rel="noopener" style="${style(
       props
     )}" href="mailto:me@yuricunha.com">Email</a>", "<a rel="noopener" style="${style(
