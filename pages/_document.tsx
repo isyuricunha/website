8a6724a6b9f82601e84dd1de9c93376c5895d756
Yuri Cunha <isyuricunha@duck.com>
2023-08-28 21:19:19 -0300

    fixed error scripting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,8 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-
 import { Analytics } from '@vercel/analytics/react';
 import { ColorModeScript } from '@chakra-ui/react';
+
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render(): React.ReactElement {
@@ -26,7 +27,6 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body id={'de_body'}>
-          <Head />
           <ColorModeScript />
           <Main />
           <Analytics />
