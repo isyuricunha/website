774ea7d5a7b4ef0309b881b46d327dc3ad27c9a6
Yuri Cunha <isyuricunha@duck.com>
2023-09-03 23:05:25 -0300

    add sitemap stream-to-promise

---
+++ generate-sitemap.js
@@ -0,0 +1,31 @@
+// Import the necessary modules
+const fs = require('fs');
+const { SitemapStream, streamToPromise } = require('sitemap');
+
+// Your Next.js routes (you may need to adjust this depending on your project structure)
+const routes = [
+  '/',
+  '/blog',
+  '/projects', // Add more routes as needed
+];
+
+// Function to generate the sitemap
+async function generateSitemap() {
+  const sitemap = new SitemapStream({ hostname: 'https://www.yuricunha.com/' }); // Replace with your website URL
+
+  // Add each route to the sitemap
+  routes.forEach((route) => {
+    sitemap.write({ url: route, changefreq: 'daily', priority: 0.7 });
+  });
+
+  sitemap.end();
+
+  // Return the sitemap as a string
+  return streamToPromise(sitemap);
+}
+
+// Generate the sitemap and save it to a file
+generateSitemap().then((sitemap) => {
+  fs.writeFileSync('./public/sitemap.xml', sitemap);
+  console.log('Sitemap generated successfully!');
+});
