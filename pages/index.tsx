421a717f9718cfb8b25c368694e5e21f1071e5a4
Yuri Cunha <isyuricunha@duck.com>
2023-09-06 21:21:17 -0300

    Revert "few improvements"
    This reverts commit 8c7b9ae24ed08aad3858981da4e8a0c6b2e1d2c2.

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import { NextSeo } from 'next-seo';
 import AboutTerminal from '@/components/AboutTerminal';
-import InstallPwaButton from '@/components/InstallWPA/InstallPwaButton';
 
 const HEADER_IMAGE_SIZE = '250px'; // Use constants for repeated values
 const HEADER_BORDER_RADIUS = '2xl';
@@ -22,9 +21,6 @@ const HEADER_BORDER_RADIUS = '2xl';
 export default function Home(): React.ReactElement {
   const [imageLoad, setImageLoad] = useState(false);
   const bp = useBreakpoint();
-
-  const isHeaderVisible = !['base', 'sm'].includes(bp); // Use constants for readability
-
   return (
     <>
       <NextSeo title="" />
@@ -37,6 +33,8 @@ export default function Home(): React.ReactElement {
         mx="auto"
         pt={{ base: '28', sm: '14', md: '16', xl: '20' }}
       >
+        {/* Im not actually too sure why this needs to be here, but without this additional flex
+        the body doesn't begin at the top of the page... */}
         <Flex
           direction="column"
           justifyContent={{ base: 'center', md: 'flex-start' }}
@@ -52,17 +50,17 @@ export default function Home(): React.ReactElement {
           >
             <Skeleton
               isLoaded={imageLoad}
-              boxSize={HEADER_IMAGE_SIZE}
-              borderRadius={HEADER_BORDER_RADIUS}
+              boxSize="250px"
+              borderRadius="2xl"
               m="auto"
             >
               <Image
                 flexGrow={3}
-                borderRadius={HEADER_BORDER_RADIUS}
-                boxSize={HEADER_IMAGE_SIZE}
+                borderRadius="2xl"
+                boxSize="250px"
                 src="./static/images/profile.jpeg"
                 objectFit="cover"
-                alt="Yuri Cunha - Database Administrator and Developer" // Provide a more descriptive alt text
+                alt="Yuri Cunha"
                 onLoad={() => setImageLoad(true)}
               />
               <Analytics />
@@ -96,11 +94,13 @@ export default function Home(): React.ReactElement {
                 fontSize="xl"
                 mt={2}
               >
+                {/* Introdução */}
                 Welcome to my website! Here, I showcase some of my{' '}
                 <Link href="/projects" passHref>
                   <ChakraLink>projects</ChakraLink>
                 </Link>{' '}
-                and experiment with various things. You can explore the{' '}
+                and experiment with various things. {/* Links para Páginas */}
+                You can explore the{' '}
                 <Link href="/links" passHref>
                   <ChakraLink>websites</ChakraLink>
                 </Link>{' '}
@@ -113,14 +113,13 @@ export default function Home(): React.ReactElement {
                   <ChakraLink>blogs</ChakraLink>
                 </Link>{' '}
                 for some interesting reads. I am a Database Administrator with
-                over 5{/*new Date().getFullYear() - 2020 */} years of
-                experience. I&apos;m also a student who dedicates most of my
-                free time to open-source projects, websites, and software
-                development.
+                over {new Date().getFullYear() - 2020} years of experience.
+                I&apos;m also a student who dedicates most of my free time to
+                open-source projects, websites, and software development.
               </chakra.p>
             </Flex>
           </Flex>
-          {isHeaderVisible && <AboutTerminal />}
+          {!['base', 'sm'].includes(bp) && <AboutTerminal />}
         </Flex>
       </Box>
     </>
