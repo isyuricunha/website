62c43489a8dacf12c7ab962e9a32118196a3cb44
Yuri Cunha <isyuricunha@duck.com>
2023-09-11 19:48:45 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,10 +11,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link
-            rel="shortcut icon"
-            href="https://www.yuricunha.com/static/images/yuricunha-logo.png"
-          />
+          <link rel="shortcut icon" href="./static/images/yuricunha-logo.png" />
           <link rel="canonical" href="https://www.yuricunha.com/" />
           <link
             href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap"
