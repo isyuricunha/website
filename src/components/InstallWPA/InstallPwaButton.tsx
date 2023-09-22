5382be49588d75ce9ef0fec2d6f192530aad8b1b
Yuri Cunha <isyuricunha@duck.com>
2023-09-22 01:06:32 -0300

    1.2.8: pwa update

--- src/components/InstallWPA/InstallPwaButton.tsx
+++ src/components/InstallWPA/InstallPwaButton.tsx
@@ -4,20 +4,32 @@ const InstallPwaButton = () => {
   const [deferredPrompt, setDeferredPrompt] = useState(null);
 
   useEffect(() => {
-    window.addEventListener('beforeinstallprompt', (e) => {
+    const handleBeforeInstallPrompt = (e) => {
       e.preventDefault();
       setDeferredPrompt(e);
-    });
+    };
+
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        'beforeinstallprompt',
+        handleBeforeInstallPrompt
+      );
+    };
   }, []);
 
   const handleInstallClick = () => {
     if (deferredPrompt) {
       deferredPrompt.prompt();
+
       deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
-          // Removed the console.log statement
-          // console.log('User accepted the install prompt');
+          // A instalação foi aceita pelo usuário, você pode adicionar lógica adicional aqui se necessário
+          console.log('O usuário aceitou a instalação da PWA.');
         }
+
+        // Reinicialize deferredPrompt após o prompt
         setDeferredPrompt(null);
       });
     }
@@ -26,7 +38,7 @@ const InstallPwaButton = () => {
   return (
     <div>
       {deferredPrompt && (
-        <button onClick={handleInstallClick}>Install PWA</button>
+        <button onClick={handleInstallClick}>Instalar PWA</button>
       )}
     </div>
   );
