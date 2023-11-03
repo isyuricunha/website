cd41098cd07331bf74c3e5e01e50d7ad38d6aec0
Yuri Cunha <isyuricunha@duck.com>
2023-11-02 21:24:02 -0300

    changing image and resize

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,7 @@ export default function Home(): React.ReactElement {
                 flexGrow={3}
                 borderRadius="2xl"
                 boxSize="250px"
-                src="./static/images/IMG_20230903_092527.jpg"
+                src="./static/images/me-wild.jpg"
                 objectFit="cover"
                 alt="Yuri Cunha"
                 onLoad={() => setImageLoad(true)}
