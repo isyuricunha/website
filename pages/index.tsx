a9bc621109a6d7a4a8dcbd21e5589d5cbaf212f5
Yuri Cunha <isyuricunha@duck.com>
2024-01-26 01:54:13 -0300

    Update image URLs

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,7 @@ export default function Home(): React.ReactElement {
                 flexGrow={3}
                 borderRadius="2xl"
                 boxSize="250px"
-                src="./static/images/toMe/yuricunha-anime.jpg"
+                src="./static/images/toMe/me.jpg"
                 objectFit="cover"
                 alt="Yuri Cunha"
                 onLoad={() => setImageLoad(true)}
