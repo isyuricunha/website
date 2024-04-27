56737add7baee78fbd22d860e414b8234f7dca50
Yuri Cunha <isyuricunha@duck.com>
2024-04-27 08:29:52 -0300

    Fix typo in CurrentlyPlaying component's phrase in MusicLayouts.tsx

--- src/components/MusicLayouts/MusicLayouts.tsx
+++ src/components/MusicLayouts/MusicLayouts.tsx
@@ -95,7 +95,7 @@ export const CurrentlyPlaying = ({
   return (
     <Flex direction="column" alignItems="center" width="full" mx="auto">
       <LineHeading mb="4">
-        <Text>Hacking mind's mainframe, rewiring rhythm.</Text>
+        <Text>Hacking mind&apos;s mainframe, rewiring rhythm.</Text>
       </LineHeading>
       {song?.isPlaying ? (
         <SongCard song={song} titleCard isPlaying={song.isPlaying} />
