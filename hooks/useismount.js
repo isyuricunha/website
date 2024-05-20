02eeb499daa7096fc3de4ba417cbe54a1f3496dd
Yuri Cunha <isyuricunha@duck.com>
2024-05-20 17:55:55 -0300

    Revert "/about create - not public"
    This reverts commit c3e2974cbfbf18160a61eb9960ed7d2f26947ecf.

--- hooks/useismount.js
+++
@@ -1,9 +0,0 @@
-import { useEffect, useRef } from "react";
-
-export default function useIsMount() {
-  const isMountRef = useRef(true);
-  useEffect(() => {
-    isMountRef.current = false;
-  }, []);
-  return isMountRef.current;
-}
