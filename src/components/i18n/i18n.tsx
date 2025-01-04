6e3b20eb87c69b09727baa3633442c2e0a573d61
Yuri <isyuricunha@duck.com>
2025-01-04 18:10:35 -0300

    add i18n api to new langs
    add 5 new langs
    
    Currently, the website supports 16 languages, and with this commit, I’ve added 5 more, further expanding the linguistic diversity. The supported languages now are:
    
    Czech (cs)
    German (de)
    English (en)
    Spanish (es)
    French (fr)
    Hindi (hi)
    Hungarian (hu)
    Italian (it)
    Japanese (ja)
    Korean (ko)
    Dutch (nl)
    Filipino (ph)
    Portuguese (pt)
    Russian (ru)
    Serbian (sr)
    Chinese (zh)
    With today’s additions, the new supported languages are Czech (cs), Hungarian (hu), Dutch (nl), Filipino (ph), and Serbian (sr).

--- src/components/i18n/i18n.tsx
+++ src/components/i18n/i18n.tsx
@@ -4,30 +4,44 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector'; // Importe o detector de idioma
 
+import csTranslation from '../../../src/locales/cs.json';
+import deTranslation from '../../../src/locales/de.json';
 import enTranslation from '../../../src/locales/en.json';
-import ptTranslation from '../../../src/locales/pt.json';
-import zhTranslation from '../../../src/locales/zh.json';
+import esTranslation from '../../../src/locales/es.json';
 import frTranslation from '../../../src/locales/fr.json';
+import hiTranslation from '../../../src/locales/hi.json';
+import huTranslation from '../../../src/locales/hu.json';
 import itTranslation from '../../../src/locales/it.json';
 import jaTranslation from '../../../src/locales/ja.json';
-import hiTranslation from '../../../src/locales/hi.json';
+import koTranslation from '../../../src/locales/ko.json';
+import nlTranslation from '../../../src/locales/nl.json';
+import phTranslation from '../../../src/locales/ph.json';
+import ptTranslation from '../../../src/locales/pt.json';
 import ruTranslation from '../../../src/locales/ru.json';
-import esTranslation from '../../../src/locales/es.json';
+import srTranslation from '../../../src/locales/sr.json';
+import zhTranslation from '../../../src/locales/zh.json';
 
 i18n
   .use(LanguageDetector) // Use o detector de idioma
   .use(initReactI18next)
   .init({
     resources: {
+      cs: { translation: csTranslation },
+      de: { translation: deTranslation },
       en: { translation: enTranslation },
-      pt: { translation: ptTranslation },
-      zh: { translation: zhTranslation },
+      es: { translation: esTranslation },
       fr: { translation: frTranslation },
+      hi: { translation: hiTranslation },
+      hu: { translation: huTranslation },
       it: { translation: itTranslation },
       ja: { translation: jaTranslation },
-      hi: { translation: hiTranslation },
+      ko: { translation: koTranslation },
+      nl: { translation: nlTranslation },
+      ph: { translation: phTranslation },
+      pt: { translation: ptTranslation },
       ru: { translation: ruTranslation },
-      es: { translation: esTranslation },
+      sr: { translation: srTranslation },
+      zh: { translation: zhTranslation },
     },
     fallbackLng: 'en', // idioma de fallback
     detection: {
