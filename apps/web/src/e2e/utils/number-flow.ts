12ea794002afe980f56d870cbbc1fb01c41df6bb
Yuri <isyuricunha@duck.com>
2025-04-25 17:13:30 -0300

    refactor(web): directly return in getNumberFlow utility

--- apps/web/src/e2e/utils/number-flow.ts
+++ apps/web/src/e2e/utils/number-flow.ts
@@ -3,7 +3,5 @@ import type { Locator } from '@playwright/test'
 export const getNumberFlow = async (locator: Locator) => {
   // @ts-expect-error -- internal property
 
-  const ariaLabel = await locator.evaluate((flow) => flow._internals.ariaLabel)
-
-  return ariaLabel
+  return await locator.evaluate((flow) => flow._internals.ariaLabel)
 }
