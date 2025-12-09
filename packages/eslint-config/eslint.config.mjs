fdcee3c3949a878f78463054a8b725700048fca2
Yuri <isyuricunha@duck.com>
2025-12-09 18:03:02 -0300

    chore: update config files and fix docs linting
    Update eslint config to ignore bundled files.
    
    Add NEXT_PUBLIC_FLAG_GEMINI to turbo.json and format.
    
    Fix unsafe return lint errors in docs.

--- packages/eslint-config/eslint.config.mjs
+++ packages/eslint-config/eslint.config.mjs
@@ -1,12 +1,17 @@
 import tszhong0411 from './dist/index.js'
 
-export default tszhong0411({
-  project: './tsconfig.json',
-  tsconfigRootDir: import.meta.dirname,
-  react: true,
-  next: true,
-  playwright: true,
-  testingLibrary: true,
-  turbo: true,
-  typescript: true
-})
+export default tszhong0411(
+  {
+    project: './tsconfig.json',
+    tsconfigRootDir: import.meta.dirname,
+    react: true,
+    next: true,
+    playwright: true,
+    testingLibrary: true,
+    turbo: true,
+    typescript: true
+  },
+  {
+    ignores: ['eslint.config.bundled_*.mjs']
+  }
+)
