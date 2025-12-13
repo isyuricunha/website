b7e6a818b96bfba13f7b219a65d2a73a8edd6251
Yuri <isyuricunha@duck.com>
2025-12-13 00:18:59 -0300

    chore(web): remove unused placeholder and example files
    remove unused ai placeholder components and examples
    
    remove unused skeleton/loading-skeleton components

--- apps/web/src/components/ai/content-generation-panel.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import type * as React from 'react'
-
-type ContentGenerationPanelProps = React.ComponentProps<'div'>
-
-export default function ContentGenerationPanel(props: ContentGenerationPanelProps) {
-  return <div {...props} />
-}
--- apps/web/src/components/ai/enhanced-chat-interface.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import type * as React from 'react'
-
-type EnhancedChatInterfaceProps = React.ComponentProps<'div'>
-
-export default function EnhancedChatInterface(props: EnhancedChatInterfaceProps) {
-  return <div {...props} />
-}
