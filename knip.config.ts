52bbbf50367a6daa1e5715b7c089d3b0d45eefc7
Yuri <isyuricunha@duck.com>
2025-12-12 22:57:51 -0300

    chore(release): refine knip dependency ignores

--- knip.config.ts
+++ knip.config.ts
@@ -14,8 +14,6 @@ const config: KnipConfig = {
   ignoreDependencies: [
     'prettier-plugin-*',
     'sharp',
-    // TailwindCSS v4 is not detectable currently
-    'tailwindcss',
     // Can't detect `pnpm with-env tsx`
     'tsx'
   ],
@@ -40,11 +38,11 @@ const config: KnipConfig = {
     },
     'packages/eslint-config': {
       // @see https://github.com/francoismassart/eslint-plugin-tailwindcss/issues/325
-      ignoreDependencies: ['@eslint/config-inspector', 'eslint-plugin-tailwindcss']
+      ignoreDependencies: ['eslint-plugin-tailwindcss']
     },
     'packages/ui': {
       // @see https://github.com/shadcn-ui/ui/issues/4792
-      ignoreDependencies: ['@radix-ui/react-context', '@tailwindcss/typography']
+      ignoreDependencies: ['@radix-ui/react-context']
     }
   }
 }
