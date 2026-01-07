2242037a543031724caa08166aab924958210d5c
Yuri Cunha <isyuricunha@duck.com>
2026-01-07 12:23:10 +0000

    style(ui): normalize select viewport classes

--- packages/ui/src/select.tsx
+++ packages/ui/src/select.tsx
@@ -90,7 +90,7 @@ const SelectContent = (props: SelectContentProps) => {
           className={cn(
             'p-1',
             position === 'popper' &&
-              'h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]'
+              'h-(--radix-select-trigger-height) w-full min-w-(--radix-select-trigger-width)'
           )}
         >
           {children}
