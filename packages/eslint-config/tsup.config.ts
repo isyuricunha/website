4bdf7c1da86e150dedc99550e941b029ae5afc27
Yuri <isyuricunha@duck.com>
2026-05-25 14:35:36 -0300

    fix(dev): prevent root dev startup prompts

--- packages/eslint-config/tsup.config.ts
+++ packages/eslint-config/tsup.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'tsup'
 
+const isWatchMode = process.argv.includes('--watch')
+
 export default defineConfig({
   entry: ['src/**/*.ts'],
   dts: true,
   format: ['esm'],
   target: 'esnext',
-  clean: true
+  clean: !isWatchMode
 })
